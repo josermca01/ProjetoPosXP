1121e3b1cb575a90305ac0c18ba21c5914aa5f51
josermca01 <josermca01@gmail.com>
2026-03-27 02:08:37 -0300

    inicio

---
+++ src/main/java/API/ProjetoPosXP/ProjetoPosXpApplication.java
@@ -0,0 +1,13 @@
+package API.ProjetoPosXP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoPosXpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoPosXpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/API/ProjetoPosXP/ProjetoPosXpApplicationTests.java
@@ -0,0 +1,13 @@
+package API.ProjetoPosXP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoPosXpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
